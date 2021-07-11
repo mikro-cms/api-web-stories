8d64e9b779c1b3e601900c025cafef0fabcc104a
Musa Sutisna <mus4.sutisn4@gmail.com>
2021-07-11 18:55:27 +0700

    Add locale

---
+++ locale/en-US/index.js
@@ -0,0 +1,12 @@
+module.exports = {
+  'webstories': {
+    'content_required': 'status content could not be empty',
+    'font_required': 'status font could not be empty',
+    'image_format': 'status only allowed file with image type',
+    'background_required': 'status background could not be empty',
+    'caption_required': 'status caption could not be empty',
+    'label_not_found': 'could not found webstories label',
+    'add_status_success': 'successful add new status',
+    'add_status_failed': 'failed to add new status'
+  }
+};
